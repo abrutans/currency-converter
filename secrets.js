19b26d47ace55cbf39996b510df796d84b6323e9
Andris Brutans <andrisbrutans@outlook.com>
2025-04-21 18:30:13 +0200

    UI REWORK

---
+++ secrets.js
@@ -0,0 +1,5 @@
+
+ const apicall = 'https://v6.exchangerate-api.com/v6/8182e25cd75229ff4ba1339b/pair/'
+
+
+export default secrets;
--- src/script.js
+++ src/script.js
@@ -3,17 +3,10 @@ const inputAmount = document.getElementById('amount-input');
 const reset = document.getElementById('reset-btn')
 const swap = document.getElementById('swap-currencies')
 
-
-
-
-
-
-
 //* map the selection template to select input
 const template = document.getElementById('currency-options')
 const currencyFromSelect = document.getElementById('currency-from')
 const currencyToSelect = document.getElementById('currency-to')
-
 
 function populateCurrencyDropdowns(){
     const options = template.content.cloneNode(true);
